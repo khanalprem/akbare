ef2f1b2584005058ab4da8e211488467309b682a
prem prasad khanal <khnl.prem@gmail.com>
2021-03-20 16:29:13 +0545

    prodcut detail page multislider added

--- js/akabare_kitchen.js
+++ js/akabare_kitchen.js
@@ -179,16 +179,111 @@ Primary use: Ecommerce
   });
 
   // Recommend Slider
-  $('.akabare-product-slider').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    arrows: true,
-    dots: false,
-    autoplay: true,
-  });
+  // $('.akabare-product-slider').slick({
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   adaptiveHeight: true,
+  //   arrows: true,
+  //   dots: false,
+  //   autoplay: true,
+  // });
 
+  function slickConfig() {
+    var product = $('.ps-product--detail');
+    if (product.length > 0) {
+      var primary = product.find('.ps-product__gallery'),
+        second = product.find('.ps-product__variants'),
+        vertical = product.find('.ps-product__thumbnail').data('vertical');
+      primary.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.ps-product__variants',
+        fade: true,
+        dots: false,
+        infinite: false,
+        arrows: primary.data('arrow'),
+        prevArrow: "<a href='#'><i class='fa fa-angle-left'></i></a>",
+        nextArrow: "<a href='#'><i class='fa fa-angle-right'></i></a>",
+      });
+      second.slick({
+        slidesToShow: second.data('item'),
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: second.data('arrow'),
+        focusOnSelect: true,
+        prevArrow: "<a href='#'><i class='fa fa-angle-up'></i></a>",
+        nextArrow: "<a href='#'><i class='fa fa-angle-down'></i></a>",
+        asNavFor: '.ps-product__gallery',
+        vertical: vertical,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              arrows: second.data('arrow'),
+              slidesToShow: 4,
+              vertical: false,
+              prevArrow: "<a href='#'><i class='fa fa-angle-left'></i></a>",
+              nextArrow: "<a href='#'><i class='fa fa-angle-right'></i></a>",
+            },
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              arrows: second.data('arrow'),
+              slidesToShow: 4,
+              vertical: false,
+              prevArrow: "<a href='#'><i class='fa fa-angle-left'></i></a>",
+              nextArrow: "<a href='#'><i class='fa fa-angle-right'></i></a>",
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 3,
+              vertical: false,
+              prevArrow: "<a href='#'><i class='fa fa-angle-left'></i></a>",
+              nextArrow: "<a href='#'><i class='fa fa-angle-right'></i></a>",
+            },
+          },
+        ],
+      });
+    }
+  }
+  function slickConfig() {
+    $('.akabare-product-gallery').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.akabare-product-thumbnail',
+    });
+    $('.akabare-product-thumbnail').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.akabare-product-gallery',
+      dots: false,
+      arrows: true,
+      centerMode: true,
+      focusOnSelect: true,
+      margin: 5,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+      ],
+    });
+  }
+  slickConfig();
   // Trending Slider
   $('.trending-slider').slick({
     centerPadding: '30px',
